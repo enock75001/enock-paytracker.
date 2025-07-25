f6e2ddb90d658315ce2484c12f344974c580695f
ENOCK DE FRANCKY <franckenock78@gmail.com>
2025-07-25 12:17:39 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: 'paytracker-gfw4j',
+  appId: '1:766910851675:web:4f9c1445e69fb1fc576d7e',
+  storageBucket: 'paytracker-gfw4j.firebasestorage.app',
+  apiKey: 'AIzaSyD-fbIQ9nMxnfxjCXRg1vy8jAaopvht0Jw',
+  authDomain: 'paytracker-gfw4j.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '766910851675',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
