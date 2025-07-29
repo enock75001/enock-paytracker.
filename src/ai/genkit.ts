70cbd8bb26dd9de5ec9ca24d633af0657790fc86
ENOCK DE FRANCKY <franckenock78@gmail.com>
2025-07-29 07:14:55 +0000

    la fonctionallité IA ne fonction pas quand je publie mon site sur mon do

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,13 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import 'dotenv/config';
 
 export const ai = genkit({
-  plugins: [googleAI({apiVersion: 'v1beta'})],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+      apiVersion: 'v1beta',
+    }),
+  ],
   model: 'googleai/gemini-1.5-flash-latest',
 });
